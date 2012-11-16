394f77361660e634de80025439e18ad38d93a839
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2012-11-16 20:51:38 +0100

    removed knockout.js
    chrome extension security policy
    preparations for localStorage access

--- js/notes.js
+++ js/notes.js
@@ -4,18 +4,6 @@ var Note = function(header, content){
 }
 
 $(document).ready(function(){
-  $('#notes').each(function(){
-    var viewModel = {
-      notes: [
-        new Note("Hello", "World!"),
-        new Note("Hello", "You!")
-      ],
-      showRenderTimes: ko.observable(false)
-    };
-
-    ko.applyBindings(viewModel);
-  });
-
   $('.note').on('click', function(){
     $('.note [contenteditable=true]').removeAttr('contenteditable');
     $(this).attr('contenteditable', 'true');
