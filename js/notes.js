827b0c96f0111519b75817b7020eb57c9e6c4ae3
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2012-11-17 08:28:00 +0100

    save note

--- js/notes.js
+++ js/notes.js
@@ -1,6 +1,12 @@
-var Note = function(header, content){
+var Note = function(id, header, content){
+  this.id = id;
   this.header = header;
   this.content = content;
+
+  this.save = function(){
+    var storage = window['localStorage'];
+    storage[this.id] = JSON.stringify(this);
+  }
 }
 
 Note.all = function(){
@@ -9,7 +15,7 @@ Note.all = function(){
 
   for(key in storage){
     item = JSON.parse(storage[key]);
-    note = new Note(item['header'], item['content']);
+    note = new Note(key, item['header'], item['content']);
     collection.push(note)
   }
 
@@ -20,5 +26,14 @@ $(document).ready(function(){
   $('.note').on('click', 'h1, p', function(){
     $('[contenteditable=true]').removeAttr('contenteditable');
     $(this).attr('contenteditable', 'true');
+  });
+
+  $('.note button').on('click', function(){
+    note_element = $(this).parent();
+    id = note_element.attr('data-id');
+    header = note_element.find('h1').text();
+    content = note_element.find('p').text();
+    note = new Note(id, header, content);
+    note.save();
   });
 });
